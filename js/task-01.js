78a1397098cc90ef3247a0e8bf0934dc1b16793b
Reuelle <ruel.paya@gmail.com>
2024-02-03 14:52:56 +0800

    task 4

--- js/task-01.js
+++ js/task-01.js
@@ -1,7 +1,7 @@
 let categories  = document.getElementsByClassName("item");
 console.log("number of categories" + categories.length);
 
-for(let i = 0; i < categories.length; i++);{
+for(let i = 0; i < categories.length; i++) {
     console.log(`Category: ${categories[i].firstElementChild.innerHTML}`);
     console.log(`Elements: ${categories[i].firstElementChild.nextElementSibling.children.length}`);
 }
