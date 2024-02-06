508840bbd84450326cc1ed912f91054949c48bfc
Reuelle <ruel.paya@gmail.com>
2024-02-06 21:39:00 +0800

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,6 +3,6 @@
 const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById('name-output');
 
-nameInput.addEventListener('input',function() {
+nameInput.addEventListener('input', function() {
     nameOutput.textContent = nameInput.value.trim() || 'Anonymous';
 });
