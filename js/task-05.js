3c9d3ae0dcfa0ebf42a928fe0ec1d742bfc1b63d
Ruel <ruel.paya@gmail.com>
2024-02-06 13:26:42 +0800

    Update task-05.js

--- js/task-05.js
+++ js/task-05.js
@@ -3,5 +3,5 @@ const nameInput = document.getElementById("name-input");
 const nameOutput = document.getElementById('name-output');
 
 nameInput.addEventListener('input',() => {
-nameOutput.textContent = nameInput.value.trim  || 'Anonymous';
+nameOutput.textContent = nameInput.value.trim()  || 'Anonymous';
 });
