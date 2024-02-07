e8e9896864054ddca05bdf4d7b2d770a739ec0a0
Ruel <ruel.paya@gmail.com>
2024-02-07 13:14:44 +0800

    task-08.js

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,13 @@
+const loginForm = document.querySelector('.login-form');
 
+    loginForm.addEventListener('submit', (event) => {
+        event.preventDefault();
+        const formData = new FormData(loginForm);
+      const formValues = Object.fromEntries(formData.entries())
+      if (Object.values(formValues).some(value => !value.trim())) {
+        alert('All fields must be filled in.');
+        return;
+      }
+      console.log(formValues);
+      loginForm.reset();
+    });
--- js/task-10.js
+++ js/task-10.js
@@ -1,5 +1,30 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215)
-    .toString(16)
-    .padStart(6, 0)}`;
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, '0')}`;
 }
+
+const controls = document.getElementById('controls');
+const createButton = controls.querySelector('[data-create]');
+const destroyButton = controls.querySelector('[data-destroy]');
+const boxesContainer = document.getElementById('boxes');
+
+createButton.addEventListener('click', () => {
+  const amount = +controls.querySelector('input').value;
+  createBoxes(amount);
+});
+
+destroyButton.addEventListener('click', destroyBoxes);
+
+function createBoxes(amount) { destroyBoxes();
+  let size = 30;
+  for (let i = 0; i < amount; i++) {
+    const box = document.createElement('div');
+    box.classList.add('box');
+    box.style.width = `${size}px`;
+    box.style.height = `${size}px`;
+    box.style.backgroundColor = getRandomHexColor();
+    boxesContainer.appendChild(box);
+    size += 10; // Increase size for the next box
+  }
+}
+
+function destroyBoxes() {  boxesContainer.innerHTML = '';}
