c24c9f7202d1018cfd4fe5141a9dc885151e8665
Ruel <ruel.paya@gmail.com>
2024-02-07 13:27:15 +0800

    Update task-06.js

--- js/task-06.js
+++ js/task-06.js
@@ -3,9 +3,9 @@ validationInput.addEventListener('blur', function() {
     const expectedLength = parseInt(validationInput.getAttribute('data-length'));
     if (validationInput.value.length === expectedLength) {
         validationInput.classList.remove('invalid');
-        validationInput.classList.add('valid');
+        validationInput.classList.add ('valid');
       } else {
         validationInput.classList.remove('valid');
-        validationInput.classList.add('invalid');
+        validationInput.classList.add ('invalid');
     }
 });
